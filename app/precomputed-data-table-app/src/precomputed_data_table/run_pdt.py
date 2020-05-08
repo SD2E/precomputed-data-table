a9e714baa64301d86ff0067210f29c011af0482b
Robert Moseley <robert.moseley@duke.edu>
2020-05-08 10:57:57 -0400

    add setup files and unit tests for grabbing latests data frames and checking data statuses

---
+++ app/precomputed-data-table-app/src/precomputed_data_table/run_pdt.py
@@ -0,0 +1,94 @@
+"""
+run all parts of the Precomputed Data Table pipeline to gather, analyze, save data;
+make summaries of the data, and make a record of the data product.
+
+to see arguments and descriptions:
+python run_pdt.py --help
+example command:
+python run_pdt.py "YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208" ../pdt_output/ hpc_path
+
+:authors: Robert C. Moseley (robert.moseley@duke.edu)
+"""
+
+import argparse
+import os
+import json
+from datetime import datetime
+
+
+def get_latest_er(exp_ref, dc_dir):
+
+    all_er_dirs = os.listdir(dc_dir)
+    latest_dirs_dict = {x.split('_')[1]: x for x in sorted(all_er_dirs)}
+
+    if exp_ref in latest_dirs_dict.keys():
+        exp_ref_dir = latest_dirs_dict[exp_ref]
+        return exp_ref_dir
+    else:
+        no_er_dir_msg = 'Experimental Reference not in Data Converge'
+        return no_er_dir_msg
+
+
+def return_er_record_path(er_dir):
+
+    record_json = 'record.json'
+    files = os.listdir(er_dir)
+    if record_json not in files:
+        return 'Experimental Reference status can not be determined. No record.json file found'
+    else:
+        return os.path.join(er_dir, record_json)
+
+
+def check_er_status(record_json):
+
+    out_status = {}
+    total_ok = 0
+
+    with open(record_json, 'r') as j:
+        record_status = json.load(j)['status']
+        for data_type, data_status in record_status.items():
+            if 'ok' in data_status:
+                out_status[data_type] = data_status
+                total_ok += 1
+            else:
+                out_status[data_type] = data_status
+
+        if total_ok < len(record_status.keys()):
+            return 'Data status NOT 100% ok:\n{}'.format(out_status)
+        elif total_ok == len(record_status.keys()):
+            return 'Data status 100% ok:\n{}'.format(out_status)
+
+
+# def main(exp_ref, out_dir, tacc_path_type, archive_system):
+def main(exp_ref, out_dir):
+    # make a new dir for output
+    now = datetime.now()
+    datetime_stamp = now.strftime('%Y%m%d%H%M%S')
+    out_dir = os.path.abspath(out_dir)
+    out_dir = os.path.join(out_dir, "dc_{0:s}_{1:s}".format(exp_ref, datetime_stamp))
+    print("making directory... ", out_dir)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
+
+    data_converge_dir = '/home/jupyter/sd2e-projects/sd2e-project-43/test'
+
+    # get latest data converge product for ER
+    er_dir = get_latest_er(exp_ref, data_converge_dir)
+    # Check status of data in ER's record.json file
+    check_er_status(return_er_record_path(er_dir))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("experiment_ref", help="experiment reference from data science table")
+    parser.add_argument("output_dir", help="directory where to write the output files")
+    # parser.add_argument("tacc_path_type", help="either 'hpc_path' or 'jupyter_path'", )
+    # parser.add_argument("-a", "--archive_system", help="tacc archive system, ex data-sd2e-projects.sd2e-project-45")
+    args = parser.parse_args()
+    arg_exp_ref = args.experiment_ref
+    arg_out_dir = args.output_dir
+    # arg_tacc_path_type = args.tacc_path_type
+    # arg_archive_system = args.archive_system
+
+    # main(arg_exp_ref, arg_out_dir, arg_tacc_path_type, arg_archive_system)
+    main(arg_exp_ref, arg_out_dir)
---
+++ app/precomputed-data-table-app/tests/run_pdt_test.py
@@ -0,0 +1,70 @@
+"""
+test running of main pdt script
+
+:authors: Robert C. Moseley (robert.moseley@duke.edu)
+"""
+
+from precomputed_data_table.run_pdt import *
+import pytest
+
+
+class TestRunPDT(object):
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.path_to_good_er_dirs = 'app/precomputed-data-table-app/tests/data/good_er_dirs'
+        self.path_to_bad_er_dirs = 'app/precomputed-data-table-app/tests/data/bad_er_dirs'
+
+    @pytest.fixture
+    def exp_ref_exists(self):
+        '''Returns an existing experiment reference in the tests/data/good_er_dirs'''
+        return 'YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208'
+
+    @pytest.fixture
+    def exp_ref_does_not_exist(self):
+        '''Returns an experiment reference not in the tests/data/good_er_dirs'''
+        return 'YeastSTATES-CRISPR-Short-Duration-Time-Series-19870430'
+
+    @pytest.fixture
+    def good_exp_ref_dir(self):
+        '''Returns an experiment reference directory with correct files'''
+        dir_path = os.path.join(self.path_to_good_er_dirs,
+                                'dc_YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208_20200328030858')
+        return dir_path
+
+    def test_latest_er_dir_exists(self, exp_ref_exists):
+        expected_er_dir = 'dc_YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208_20200414095504'
+        er_dir = get_latest_er(exp_ref_exists, self.path_to_good_er_dirs)
+        assert er_dir == expected_er_dir
+
+    def test_latest_er_dir_does_not_exist(self, exp_ref_does_not_exist):
+        expected_msg = 'Experimental Reference not in Data Converge'
+        er_dir = get_latest_er(exp_ref_does_not_exist, self.path_to_good_er_dirs)
+        assert er_dir == expected_msg
+
+    def test_record_json_exists(self, good_exp_ref_dir):
+        expected_record_json_path = os.path.join(good_exp_ref_dir, 'record.json')
+        record_json_path = return_er_record_path(good_exp_ref_dir)
+        assert record_json_path == expected_record_json_path
+
+    def test_record_json_does_not_exist(self):
+        expected_msg = 'Experimental Reference status can not be determined. No record.json file found'
+        bad_exp_ref = 'dc_YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208_20200328030858'
+        record_json_path = return_er_record_path(os.path.join(self.path_to_bad_er_dirs, bad_exp_ref))
+        assert record_json_path == expected_msg
+
+    def test_data_status_is_good(self, good_exp_ref_dir):
+        record_json_path = os.path.join(good_exp_ref_dir, 'record.json')
+        good_status = {"fc_raw": "ok", "fc_etl": "ok", "pr": "ok"}
+        expected_msg = 'Data status 100% ok:\n{}'.format(good_status)
+        status_msg = check_er_status(record_json_path)
+        assert status_msg == expected_msg
+
+    def test_data_status_is_not_good(self):
+        bad_exp_ref = 'dc_YeastSTATES-OR-Gate-CRISPR-Dose-Response_20200423202039'
+        record_json_path = os.path.join(self.path_to_bad_er_dirs, bad_exp_ref, 'record.json')
+        bad_status = {"fc_raw": "Fail", "fc_etl": "ok. unique sample ids: 244",
+                      "pr": "ok. unique sample ids: 310"}
+        expected_msg = 'Data status NOT 100% ok:\n{}'.format(bad_status)
+        status_msg = check_er_status(record_json_path)
+        assert status_msg == expected_msg
