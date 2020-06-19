eefd0d26528c25ea21a375e356bcbb72dfe14df7
Daniel Bryce <danbryce410@gmail.com>
2020-06-19 14:02:10 -0500

    skeleton for fcs signal prediction

---
+++ app/precomputed-data-table-app/fcs_signal_prediction/src/fcs_signal_prediction/main.py
@@ -0,0 +1,9 @@
+
+from fcs_signal_prediction.predict import predict
+
+def main():
+    predict(None, None)
+
+
+if __name__ == '__main__':
+    main()
---
+++ app/precomputed-data-table-app/fcs_signal_prediction/src/fcs_signal_prediction/predict.py
@@ -0,0 +1,5 @@
+from pandas import DataFrame
+
+
+def predict_signal(df: DataFrame, experiment_identifier: str) -> DataFrame:
+    pass
