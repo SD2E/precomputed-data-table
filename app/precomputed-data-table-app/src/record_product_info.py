15fee71ce8e18bc7c47271cb00ecc1cf0949b0dd
gzheng2 <gzheng@eagc.test>
2020-07-14 16:01:09 -0400

    moved product level code to the reactor

--- app/precomputed-data-table-app/src/record_product_info.py
+++ app/precomputed-data-table-app/src/record_product_info.py
@@ -62,39 +62,6 @@ def make_hashes_for_files(out_dir, file_list):
 
     return file_list_dict
 
-
-def get_dev_git_version():
-    import os
-    stream = os.popen('git show -s --format="gitinfo: %h %ci"')
-    output = stream.read().strip()
-    if output.startswith('gitinfo:'):
-        output = output.replace('gitinfo: ', '')
-    else:
-        output = "NA"
-
-    version_info = output
-
-    return version_info
-
-
-def make_product_record(exp_ref, out_dir, dc_dir):
-
-    datetime_stamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    version_info = get_dev_git_version()
-
-    record = {
-        "precomputed_data_table version": version_info,
-        "experiment_reference": exp_ref,
-        "date_run": datetime_stamp,
-        "output_dir": out_dir,
-        "analyses": {},
-        "status_upstream": {
-            'data-converge directory': dc_dir
-        }
-    }
-
-    return record
-
 def append_record(record, files_dict, analysis, out_dir):
 
     # TODO add md5 check of dc files
