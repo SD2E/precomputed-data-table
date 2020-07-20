9641718906c564e391e571d52514cf742ebfd188
Robert Moseley <robert.moseley@duke.edu>
2020-07-20 17:38:46 -0400

    comment out plotting code

--- app/precomputed-data-table-app/src/run_fcs_signal_prediction.py
+++ app/precomputed-data-table-app/src/run_fcs_signal_prediction.py
@@ -14,14 +14,14 @@ def run_fcs_signal_prediction(exp_ref, exp_dir):
     high_control = 'CRISPR_CEN.PK2_positive_control_NOR_00_24864'
     low_control = 'CRISPR_CEN.PK2_negative_control_23970'
 
-    result, timeseries_fig, samples_and_controls_fig = main(exp_dir,
-                                                            exp_ref,
-                                                            low_control,
-                                                            high_control)
-    timeseries_fig.savefig('{}__well_timeseries_figure.png'.format(exp_ref), format='png', dpi=100)
-    samples_and_controls_fig.savefig('{}__samples_and_controls_figure.png'.format(exp_ref), format='png', dpi=100)
+    # result, timeseries_fig, samples_and_controls_fig = main(exp_dir,
+    #                                                         exp_ref,
+    #                                                         low_control,
+    #                                                         high_control)
+    # timeseries_fig.savefig('{}__well_timeseries_figure.png'.format(exp_ref), format='png', dpi=100)
+    # samples_and_controls_fig.savefig('{}__samples_and_controls_figure.png'.format(exp_ref), format='png', dpi=100)
 
-    # result = main(exp_dir, exp_ref, low_control, high_control)
+    result = main(exp_dir, exp_ref, low_control, high_control)
 
     results_fname = 'pdt_{}__fcs_signal_prediction.csv'.format(exp_ref)
 
