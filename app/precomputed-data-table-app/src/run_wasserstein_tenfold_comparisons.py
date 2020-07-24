2da964e03a7a512712680c8dc0cdfe471065e75a
Robert Moseley <robert.moseley@duke.edu>
2020-07-24 17:22:13 -0400

    changed run_wasser

--- app/precomputed-data-table-app/src/run_wasserstein_tenfold_comparisons.py
+++ app/precomputed-data-table-app/src/run_wasserstein_tenfold_comparisons.py
@@ -16,12 +16,10 @@ import wasserstein_tenfold_comparisons.wasserstein_analysis as wasser
 import wasserstein_tenfold_comparisons.TenFoldComparisons as tenfoldcomp
 
 
-def run_wasserstain_analysis(er_dir, r_dict):
+def run_wasserstain_analysis(er_dir, datafile):
 
-    for datafile in r_dict.keys():
-        wasser_df = wasser.do_analysis(er_dir, datafile)
-        r_dict[datafile]['wasserstein_dists'] = wasser_df
-    return r_dict
+    wasser_df = wasser.do_analysis(er_dir, datafile)
+    return wasser_df
 
 
 def return_fc_meta_name(er_dir):
@@ -117,16 +115,9 @@ def run_wasser_tenfold(exp_ref, exp_ref_dir):
         for data in single_results_dict.keys():
             if data == 'wasserstein_dists':
                 fname_wasser = 'pdt_{}__{}_{}_{}.csv'.format(exp_ref, datafile.split(".")[0], data, datetime)
-                results_dict = run_wasserstain_analysis(exp_ref_dir, results_dict)
+                results_dict[datafile]['wasserstein_dists'] = run_wasserstain_analysis(exp_ref_dir, datafile)
                 results_dict[datafile]['wasserstein_dists'].to_csv(fname_wasser)
                 fname_dict[full_datafile_name].append(fname_wasser)
-            # else:
-            #     fname_summary = 'pdt_{}__{}_{}_summary_{}.csv'.format(exp_ref, datafile.split(".")[0], data, datetime)
-            #     fname_params = 'pdt_{}__{}_{}_params_{}.json'.format(exp_ref, datafile.split(".")[0], data, datetime)
-            #     tenfoldcomp.save_summary(results_dict[datafile][data]['tenfold_summary'], fname_summary)
-            #     tenfoldcomp.save_params(results_dict[datafile][data]['tenfold_params'], fname_params)
-            #     fname_dict[full_datafile_name].append(fname_summary)
-            #     fname_dict[full_datafile_name].append(fname_params)
 
     for datafile, single_results_dict in results_dict.items():
         full_datafile_name = '__'.join([exp_ref, datafile])
