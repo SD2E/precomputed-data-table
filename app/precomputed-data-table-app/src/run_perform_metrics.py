d26d13aed2c42b4516645d8146a79f88669fd0dc
adeckard <anastasia.deckard@geomdata.com>
2020-08-10 16:44:30 -0400

    return job template and product patterns

--- app/precomputed-data-table-app/src/run_perform_metrics.py
+++ app/precomputed-data-table-app/src/run_perform_metrics.py
@@ -11,22 +11,40 @@ from agavepy import Agave
 
 ag = Agave.restore()
 
-# TODO: this only supports plate reader right now, so should only be called from run_pdt where
-#  dtype_confirm_dict['platereader'] == True
 
-def main(out_sys, out_dir, dc_batch_path, experiment_reference):
-
-    # if there are known experiment types or experiments refs that don't work
-    # with perform-metrics, should probably exclude them here?
+# right now perform metrics can run on these supported types
+supported_cps = ['YEAST_STATES']
+supported_protocols = ['TimeSeriesHTP']
+supported_measurement_types = ['PLATE_READER']
 
 
-    # make a directory for perform_metrics output out_sys/out_dir/perform-metrics
+def is_supported(challenge_problem, protocol, measurement_type):
+    if challenge_problem not in supported_cps:
+        return False
+    if protocol not in supported_protocols:
+        return False
+    if measurement_type not in supported_measurement_types:
+        return False
+
+
+def get_job_template(out_sys, out_dir, dc_batch_path, experiment_reference):
+    """
+    create a job template with info needed to run the perform metrics-app
+    currently just supporting one config to test it
+
+    :param out_sys: 'sd2e-projects'
+    :param out_dir: 'sd2e-project-48/complete/<exp_ref>/<datetime>'
+    :param dc_batch_path: 'agave://data-sd2e-projects.sd2e-project-43/reactor_outputs/complete/<exp_ref>/<datetime>'
+    :param experiment_reference: 'YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208'
+    :return:
+    """
+
+    # directory for perform_metrics output out_sys/out_dir/perform-metrics
     pm_dir = os.path.join(out_dir, 'perform-metrics')
     print("batch dir: ", pm_dir)
 
     # build a path to the file we want from the DC output; here we will use plate reader
-    # use a config file for platereader data we've set up to work with these experiment refs,
-    # these must be in the configs folder that is deployed with the code
+    # use a config file for platereader from configs folder that is deployed with the code
     perform_metrics_data = '{0:s}/{1:s}__platereader.csv'.format(dc_batch_path, experiment_reference)
     perform_metrics_config_json = "/perform_metrics/src/perform_metrics/configs/metrics_y4d_ts_pr_inducer.json"
 
@@ -36,7 +54,7 @@ def main(out_sys, out_dir, dc_batch_path, experiment_reference):
                     "archiveSystem": out_sys,
                     "archivePath": pm_dir,
                     "archiveOnAppError": True,
-                    "maxRunTime": "12:00:00",
+                    "maxRunTime": "2:00:00",
                     "inputs": {
 
                         "perform_metrics_data": perform_metrics_data
@@ -46,23 +64,23 @@ def main(out_sys, out_dir, dc_batch_path, experiment_reference):
                     }
                     }
 
-    job_id = ag.jobs.submit(body=job_template)['id']
-    print(batch_dir)
-    print(exp_ref, job_id)
-    print(json.dumps(job_template, indent=4))
+    product_patterns = [
+        {'patterns': ['^.*(tsv)$'],
+         'derived_from': [perform_metrics_data],
+         'derived_using': []
+         }]
+
+    return job_template, product_patterns
 
 
 if __name__ == '__main__':
-    # in prod, paths would be passed to us like:
-    # out_sys = 'sd2e-projects'
-    # out_dir = 'sd2e-project-48/complete/YeastSTATES-CRISPR-Growth-Curves-with-Plate-Reader-Optimization/20200722190009'
-    # dc_batch_path = 'agave://data-sd2e-projects.sd2e-project-43/reactor_outputs/complete/YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208/20200610192131'
-    # experiment_reference = 'YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208'
-
-    # for testing to our local directories:
-    out_sys = 'data-tacc-work-adeckard'
-    out_dir = 'perform-metrics-uat'
-    dc_batch_path = 'agave://data-sd2e-projects.sd2e-project-43/test/batch_20200713124857_master/YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208'
-    experiment_reference = 'YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208'
-
-    main(out_sys, out_dir, dc_batch_path, experiment_reference)
+    # an example of how to call this code:
+    arg_challenge_problem = 'YEAST_STATES'
+    arg_protocol = 'TimeSeriesHTP'
+    arg_measurement_type = 'PLATE_READER'
+    if is_supported(challenge_problem, protocol, measurement_type):
+        arg_out_sys = 'sd2e-projects'
+        arg_out_dir = 'sd2e-project-48/complete/YeastSTATES-CRISPR-Growth-Curves-with-Plate-Reader-Optimization/20200722190009'
+        arg_dc_batch_path = 'agave://data-sd2e-projects.sd2e-project-43/reactor_outputs/complete/YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208/20200610192131'
+        arg_experiment_reference = 'YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208'
+        my_job_template = get_job_template(out_sys, out_dir, dc_batch_path, experiment_reference)
