942417ca8c95c09d5abb226d9254fec407a690ce
Robert Moseley <robert.moseley@duke.edu>
2020-08-18 16:12:04 -0400

    updated reactor config.yml

--- app/precomputed-data-table-wasserstein/src/record_product_info.py
+++ app/precomputed-data-table-wasserstein/src/record_product_info.py
@@ -6,19 +6,6 @@ gather record product info and make data files summaries for this run of precomp
 
 import hashlib
 import os
-import json
-import pandas as pd
-
-from datetime import datetime
-# import pymongo
-#
-#
-# def get_db_conn():
-#     dbURI = 'mongodb://readonly:WNCPXXd8ccpjs73zxyBV@catalog.sd2e.org:27020/admin?readPreference=primary'
-#     client = pymongo.MongoClient(dbURI)
-#     db = client.catalog_staging
-#
-#     return db
 
 
 def make_hash(file_path):
