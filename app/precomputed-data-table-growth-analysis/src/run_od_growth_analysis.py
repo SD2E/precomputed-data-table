5a64d46d297cf06834d7746e778b71bded962a85
gzheng2 <gzheng@eagc.test>
2020-12-03 19:42:04 -0500

    pass credentials to GA app and code cleanup

--- app/precomputed-data-table-growth-analysis/src/run_od_growth_analysis.py
+++ app/precomputed-data-table-growth-analysis/src/run_od_growth_analysis.py
@@ -12,9 +12,9 @@ import json
 import argparse
 import pandas as pd
 from common import preproc
-from typing import Optional
+from typing import Optional, TypedDict
 from pandas import DataFrame
-from xplan_od_growth_analysis import analysis_frame_api
+from xplan_od_growth_analysis import credentials, analysis_frame_api
 from common import record_product_info as rpi
 
 
@@ -34,11 +34,12 @@ def grab_meta_dataframe(exp_ref, er_dir):
     return meta_df, meta_file_name
 
 
-def run_od_analysis(platereader_df, exp_ref, fcs_meta_df: Optional[DataFrame] = None):
+def run_od_analysis(creds, platereader_df, exp_ref, fcs_meta_df: Optional[DataFrame] = None):
 
     if fcs_meta_df is None:
         return_dict: analysis_frame_api.AdReturn = analysis_frame_api.augment_dataframe(od_df=platereader_df,
-                                                                                        experiment_identifier=exp_ref)
+                                                                                        experiment_identifier=exp_ref,
+                                                                                        credentials_dict=creds)
         pr_analysis_df = return_dict['od_df']
         assert isinstance(pr_analysis_df, pd.DataFrame)
         return pr_analysis_df
@@ -46,63 +47,15 @@ def run_od_analysis(platereader_df, exp_ref, fcs_meta_df: Optional[DataFrame] = 
     else:
         return_dict: analysis_frame_api.AdReturn = analysis_frame_api.augment_dataframe(od_df=platereader_df,
                                                                                         experiment_identifier=exp_ref,
-                                                                                        fcs_meta_df=fcs_meta_df)
+                                                                                        fcs_meta_df=fcs_meta_df,
+                                                                                        credentials_dict=creds)
         fcs_analysis_df = return_dict['fcs_meta_df']
         pr_analysis_df = return_dict['od_df']
         assert isinstance(pr_analysis_df, pd.DataFrame)
         assert isinstance(fcs_analysis_df, pd.DataFrame)
         return pr_analysis_df, fcs_analysis_df
 
-
-# def rows_to_replicate_groups(data_df, m_type):
-#
-#     if m_type == 'od':
-#         # drop duplicates and induction samples
-#         dup_cols = ['doubling_time', 'n0', 'inducer_type', 'well', 'experiment_id']
-#         data_drop_df = data_df.drop_duplicates(subset=dup_cols, keep='first')
-#
-#         # drop sample-specific columns
-#         drop_cols = ['_id', 'sample_id', 'replicate', 'replicate_group', 'replicate_group_string',
-#                      'timepoint', 'timepoint_unit', 'container_id',
-#                      'aliquot_id']
-#         drop_cols += [col for col in data_drop_df.columns.tolist() if 'fluor_gain' in col]
-#
-#         data_drop_df.drop(drop_cols, axis=1, inplace=True)
-#
-#         return data_drop_df
-#
-#     elif m_type == 'fc':
-#         # drop duplicates
-#         dup_cols = ['well', 'experiment_id']
-#         data_drop_df = data_df.drop_duplicates(subset=dup_cols, keep='first')
-#         # drop sample-specific columns
-#         drop_cols = ['_id', 'sample_id', 'replicate', 'replicate_group', 'replicate_group_string',
-#                      'timepoint', 'timepoint_unit', 'TX_plate_name']
-#         data_drop_df.drop(drop_cols, axis=1, inplace=True)
-#         # ***
-#         return data_drop_df
-
-
-# def run_od_analysis(exp_ref, exp_ref_dir, data_confirm_dict):
-#
-#     pr_df, pr_fname = grab_pr_dataframe(exp_ref, exp_ref_dir)
-#
-#     if data_confirm_dict['fc_raw_log10']:
-#         fcs_meta_df, fcs_meta_fname = grab_meta_dataframe(exp_ref, exp_ref_dir)
-#         pr_analysis_df, fcs_analysis_df = growth_analysis(pr_df, exp_ref, fcs_meta_df)
-#
-#         return pr_analysis_df, pr_fname, fcs_analysis_df, fcs_meta_fname
-#
-#     else:
-#         pr_analysis_df = growth_analysis(pr_df, exp_ref)
-#
-#         return pr_analysis_df, pr_fname
-
-    # make df rows = replicate groups
-    # rg_od_analysis_df = rows_to_replicate_groups(od_analysis_initial_df, 'od')
-
-
-def main(exp_ref, analysis, out_dir, data_converge_dir):
+def main(exp_ref, analysis, out_dir, data_converge_dir, arg_sift_ga_sbh_url, arg_sift_ga_sbh_user, arg_sift_ga_sbh_password, arg_sift_ga_mongo_user):
 
     # Check status of data in ER's record.json file
     path_to_record_json = preproc.return_er_record_path(data_converge_dir)
@@ -116,12 +69,19 @@ def main(exp_ref, analysis, out_dir, data_converge_dir):
     pr_df, pr_fname = grab_pr_dataframe(exp_ref, data_converge_dir)
     pr_growth_fname = 'pdt_{}__pr_growth_analysis.csv'.format(exp_ref)
 
+    creds: CredentialsDict = {
+        'SIFT_GA_SBH_URL': arg_sift_ga_sbh_url,
+        'SIFT_GA_SBH_USER': arg_sift_ga_sbh_user,
+        'SIFT_GA_SBH_PASSWORD': arg_sift_ga_sbh_password,
+        'SIFT_GA_MONGO_USER': arg_sift_ga_mongo_user
+    }
+
     if data_confirm_dict['fc_raw_log10']:
         fcs_meta_df, fcs_meta_fname = grab_meta_dataframe(exp_ref, data_converge_dir)
-        pr_analysis_df, fcs_analysis_df = run_od_analysis(pr_df, exp_ref, fcs_meta_df)
+        pr_analysis_df, fcs_analysis_df = run_od_analysis(creds, pr_df, exp_ref, fcs_meta_df)
         fcs_growth_fname = 'pdt_{}__fcs_growth_analysis.csv'.format(exp_ref)
     else:
-        pr_analysis_df = run_od_analysis(pr_df, exp_ref)
+        pr_analysis_df = run_od_analysis(creds, pr_df, exp_ref)
 
     pr_analysis_df.to_csv(pr_growth_fname, index=False)
 
@@ -138,17 +98,24 @@ def main(exp_ref, analysis, out_dir, data_converge_dir):
     with open(record_path, 'w') as jfile:
         json.dump(record, jfile, indent=2)
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--experiment_ref", help="experimental reference from data science table")
     parser.add_argument("--data_converge_dir", help="path to Data Converge directory")
     parser.add_argument("--analysis", help="analysis to run")
+    parser.add_argument("--sift_ga_sbh_url", help="URL for the SynBioHub instance")
+    parser.add_argument("--sift_ga_sbh_user", help="Username with access to the SynBioHub instance")
+    parser.add_argument("--sift_ga_sbh_password", help="Password for `GA_SBH_USER` account")
+    parser.add_argument("--sift_ga_mongo_user", help="Username with access to read experiment requests from MongoDB")
 
     args = parser.parse_args()
     arg_exp_ref = args.experiment_ref
     arg_data_converge_dir = args.data_converge_dir
     arg_analysis = args.analysis
     arg_out_dir = "."
+    arg_sift_ga_sbh_url = args.sift_ga_sbh_url
+    arg_sift_ga_sbh_user = args.sift_ga_sbh_user
+    arg_sift_ga_sbh_password = args.sift_ga_sbh_password
+    arg_sift_ga_mongo_user = args.sift_ga_mongo_user
 
-    main(arg_exp_ref, arg_analysis, arg_out_dir, arg_data_converge_dir)
+    main(arg_exp_ref, arg_analysis, arg_out_dir, arg_data_converge_dir, arg_sift_ga_sbh_url, arg_sift_ga_sbh_user, arg_sift_ga_sbh_password, arg_sift_ga_mongo_user)
