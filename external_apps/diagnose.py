bdeb68da16d41b96cec761646c09c847c82d9837
adeckard <anastasia.deckard@geomdata.com>
2021-01-26 03:01:19 -0500

    fix up paths

--- external_apps/diagnose.py
+++ external_apps/diagnose.py
@@ -10,13 +10,12 @@ app_name = "diagnose_app"
 app_id = "diagnose_app-0.1.0"
 
 
-def get_job_template(out_sys, out_dir, dc_batch_path, experiment_reference, mtype):
+def get_job_template(out_sys, pdt_path, experiment_reference, mtype):
     """
     create a job template with info needed to run the diagnose-app
 
     :param out_sys: 'sd2e-projects'
-    :param out_dir: 'sd2e-project-48/complete/<exp_ref>/<datetime>'
-    :param dc_batch_path: 'agave://data-sd2e-projects.sd2e-project-43/reactor_outputs/complete/<exp_ref>/<datetime>'
+    :param pdt_path: 'sd2e-project-48/complete/<exp_ref>/<datetime>'
     :param experiment_reference: 'YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208'
     :return:
     """
@@ -26,10 +25,9 @@ def get_job_template(out_sys, out_dir, dc_batch_path, experiment_reference, mtyp
     config_file = None
     product_patterns = None
 
-    config_file = 'pm_ys1_fc_etl.json'
-
-    if config_file:
-        out_dir = os.path.join(out_dir, 'diagnose__' + mtype)
+    # currently only supporting flow etl; will add check for all data types here
+    if mtype == 'FLOW':
+        out_dir = os.path.join(pdt_path, 'diagnose__' + mtype)
 
         job_template = {"name": app_name,
                         "appId": app_id,
@@ -44,18 +42,25 @@ def get_job_template(out_sys, out_dir, dc_batch_path, experiment_reference, mtyp
 
         # figure out input files
         data_files = list()
+        base_in_dir = os.path.join(pdt_path, '{perform-metrics__{0:s}'.format(mtype))
+        diagnose_config_path = "/diagnose/src/diagnose/configs/"
         if mtype == 'FLOW':
-            base_in_dir = '{0:s}/perform-metrics__{1:s}/'.format(dc_batch_path, mtype)
-            diagnose_exp_file = base_in_dir + 'per_sample_metric_circuit_media.tsv'.format(dc_batch_path, mtype)
-            diagnose_optional_data = base_in_dir + '{0:s}__fc_meta.csv'.format(experiment_reference)
+            diagnose_exp_file = os.path.join(base_in_dir,
+                                             'per_sample_metric_circuit_media.tsv'.format(pdt_path, mtype))
+            diagnose_optional_data = os.path.join(base_in_dir,
+                                                  '{0:s}__fc_meta.csv'.format(experiment_reference))
             job_template['inputs']['exp_file'] = diagnose_exp_file
             job_template['inputs']['diagnose_optional_metadata'] = diagnose_optional_data
             data_files = [diagnose_exp_file, diagnose_optional_data]
 
-    product_patterns = [
-        {'patterns': ['^.*(tsv)$'],
-         'derived_from': data_files,
-         'derived_using': []
-         }]
+            # figure out configuration files
+            config_file = 'pm_ys1_fc_etl.json'
+            job_template['parameters']['diagnose_config_json'] = os.path.join(diagnose_config_path, config_file)
+
+        product_patterns = [
+            {'patterns': ['^.*(tsv)$'],
+             'derived_from': data_files,
+             'derived_using': []
+             }]
 
     return job_template, product_patterns
