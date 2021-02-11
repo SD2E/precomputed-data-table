70bfd34f9ca8c07d516383abd8b0d71e4f6c6363
adeckard <anastasia.deckard@geomdata.com>
2021-02-11 04:08:21 -0500

    add config mapping for different exp refs

--- external_apps/diagnose.py
+++ external_apps/diagnose.py
@@ -9,6 +9,17 @@ import os
 app_name = "diagnose_app"
 app_id = "diagnose_app-0.1.0"
 
+map_data_type = {
+    'YeastSTATES-1-0-Time-Series-Round-1': 'ys_1.0',
+    'YeastSTATES-1-0-Time-Series-Round-1-1': 'ys_1.0',
+    'YeastSTATES-1-0-Time-Series-Round-2-0': 'ys_1.0',
+    'YeastSTATES-1-0-Time-Series-Round-3-0': 'ys_1.0',
+}
+
+map_data_file = {
+    'default': 'per_sample_metric_str_time.tsv',
+    'ys_1.0': 'per_sample_metric_circuit_media.tsv',
+}
 
 def get_job_template(out_sys, out_dir, pm_batch_path, experiment_reference, mtype):
     """
@@ -23,9 +34,16 @@ def get_job_template(out_sys, out_dir, pm_batch_path, experiment_reference, mtyp
     """
 
     job_template = None
-    config_type = None
-    config_file = None
+    data_type = None
+    data_file = None
     product_patterns = None
+
+    if experiment_reference in map_data_type.keys():
+        data_type = map_data_type[experiment_reference]
+    else:
+        data_type = 'default'
+
+    data_file = map_data_file[data_type]
 
     # currently only supporting flow etl; will add check for all data types here
     if mtype == 'FLOW':
@@ -47,7 +65,7 @@ def get_job_template(out_sys, out_dir, pm_batch_path, experiment_reference, mtyp
         base_in_dir = os.path.join(pm_batch_path, 'perform-metrics__{0:s}'.format(mtype))
         diagnose_config_path = "/diagnose/src/diagnose/configs/"
         if mtype == 'FLOW':
-            diagnose_exp_file = os.path.join(base_in_dir, 'per_sample_metric_circuit_media.tsv')
+            diagnose_exp_file = os.path.join(base_in_dir, data_file)
             diagnose_optional_data = os.path.join(base_in_dir,
                                                   '{0:s}__fc_meta.csv'.format(experiment_reference))
             job_template['inputs']['exp_file'] = diagnose_exp_file
@@ -71,7 +89,7 @@ if __name__ == '__main__':
     out_sys = 'sd2e-projects'
     out_dir = 'sd2e-project-48/complete/<exp_ref>/<datetime>'
     pm_batch_path = 'agave://data-sd2e-projects.sd2e-project-48/complete/<exp_ref>/<datetime>'
-    experiment_reference = 'YeastSTATES-CRISPR-Short-Duration-Time-Series-20191208'
+    experiment_reference = 'YeastSTATES-1-0-Time-Series-Round-3-0'
     mtype = 'FLOW'
 
     job = get_job_template(out_sys, out_dir, pm_batch_path, experiment_reference, mtype)
