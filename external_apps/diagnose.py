d71385dec14c1f337f7d5c03b93ee258cbb2ac02
adeckard <anastasia.deckard@geomdata.com>
2021-06-21 09:33:45 -0700

    add YeastSTATES-Dual-Response-CRISPR-Redesigns-Short-Duration-Time-Series-30C-Titration

--- external_apps/diagnose.py
+++ external_apps/diagnose.py
@@ -16,6 +16,7 @@ map_data_type = {
     'YeastSTATES-Doxycycline-OR-Gate-Plant-TF-Dose-Response': 'ys_annot',
     'YeastSTATES-Dual-Response-CRISPR-Short-Duration-Time-Series-30C': 'ys_annot',
     'YeastSTATES-OR-Gate-CRISPR-Dose-Response': 'ys_annot',
+    'YeastSTATES-Dual-Response-CRISPR-Redesigns-Short-Duration-Time-Series-30C-Titration': 'ys_annot',
 
     'YeastSTATES-1-0-Time-Series-Round-1': 'ys_1.0',
     'YeastSTATES-1-0-Time-Series-Round-1-1': 'ys_1.0',
